f0818e941180243ae852d9ca866516b37bfe29d2
Andres Lozano <yeisonandreslozano@gmail.com>
2023-12-21 01:22:48 -0500

    Revision 2 datatable en espanol

--- src/app/pages/gestion-pacientes/gestion-pacientes.component.ts
+++ src/app/pages/gestion-pacientes/gestion-pacientes.component.ts
@@ -1,11 +1,21 @@
-import { Component, EventEmitter, OnDestroy, OnInit, Output, ViewChild } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  OnDestroy,
+  OnInit,
+  Output,
+  ViewChild,
+} from '@angular/core';
 import { Router } from '@angular/router';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { DataTableDirective } from 'angular-datatables';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { ToastrService } from 'ngx-toastr';
 import { Subject } from 'rxjs';
-import { Paises, RegistrarPacientes } from 'src/app/models/Parametrizacion-model';
+import {
+  Paises,
+  RegistrarPacientes,
+} from 'src/app/models/Parametrizacion-model';
 import { ServiciosService } from 'src/app/services/servicios.service';
 import { environment } from 'src/environments/environment';
 import Swal from 'sweetalert2';
@@ -20,12 +30,12 @@ export class GestionPacientesComponent implements OnInit, OnDestroy {
   selected: any;
   public sex: string = '';
   public listaSex: string[] = ['Masculino', 'Femenino'];
-  listPacientes: RegistrarPacientes[] =[];
+  listPacientes: RegistrarPacientes[] = [];
 
   @Output()
-   cerrarModalPaciente: EventEmitter<boolean> =  new EventEmitter<boolean>();
+  cerrarModalPaciente: EventEmitter<boolean> = new EventEmitter<boolean>();
 
-    /**Parametros tabla */
+  /**Parametros tabla */
   //dtOptions: any = {};
   dtOptions: DataTables.Settings = {};
   dtTriggers: Subject<any> = new Subject<any>();
@@ -39,23 +49,10 @@ export class GestionPacientesComponent implements OnInit, OnDestroy {
     private modal: NgbModal,
     public router: Router,
     public toastr: ToastrService,
-    private spinner : NgxSpinnerService,
+    private spinner: NgxSpinnerService
   ) {}
 
   ngOnInit(): void {
-    this.dtOptions = {
-      pagingType: 'full_numbers',
-      searching: false,
-      pageLength: 5,
-      language: {
-        url: '//cdn.datatables.net/plug-ins/1.10.15/i18n/Spanish.json',
-      },
-      serverSide: true,
-      processing: true,
-      dom: 'Bfrtip',
-      //buttons: ['copy', 'csv', 'excel', 'print', 'pdf'],
-      responsive: true,
-    };
     this.formRegistro = this.services.cargarFormRegistroPacientes();
     if (localStorage.getItem('creado') == 'ok') {
       localStorage.removeItem('creado');
@@ -80,6 +77,7 @@ export class GestionPacientesComponent implements OnInit, OnDestroy {
         }
       );
     }
+
   }
 
   ngOnDestroy(): void {
@@ -106,54 +104,62 @@ export class GestionPacientesComponent implements OnInit, OnDestroy {
 
   registrar() {
     this.spinner.show();
-    this.services.registrarPacientes(this.formRegistro.value, localStorage.getItem('token')).subscribe(
-      (result: any) => {
-        console.log('correcot', result);
-        if(result.status == 'ACCEPTED'){
+    this.services
+      .registrarPacientes(
+        this.formRegistro.value,
+        localStorage.getItem('token')
+      )
+      .subscribe(
+        (result: any) => {
+          console.log('correcot', result);
+          if (result.status == 'ACCEPTED') {
+            this.spinner.hide();
+            this.toastr.success(
+              result.mensaje == null
+                ? 'Paciente creado correctamente'
+                : result.mensaje
+            );
+            this.formRegistro.reset();
+            this.getListPacientes();
+            this.modal.dismissAll();
+          }
+        },
+        (error) => {
+          console.log('err', error);
           this.spinner.hide();
-          this.toastr.success(result.mensaje == null ? 'Paciente creado correctamente' : result.mensaje);
-          this.formRegistro.reset();
-          this.getListPacientes();
-          this.modal.dismissAll();
-        }
-      },
-      (error) => {console.log('err', error);
-        this.spinner.hide();
-        if (error.status == 302) {
-          Swal.fire({
-            icon: 'error',
-            title: 'Error',
-            text: error.error.mensaje['message'],
-          });
-          this.toastr.info(
-            'No es posible registrar el paciente con este número de documento',
-            error.error.obj.documento
-          );
-        }
+          if (error.status == 302) {
+            Swal.fire({
+              icon: 'error',
+              title: 'Error',
+              text: error.error.mensaje['message'],
+            });
+            this.toastr.info(
+              'No es posible registrar el paciente con este número de documento',
+              error.error.obj.documento
+            );
+          }
 
-        if(error.status == 403){
-          this.spinner.hide();
-          localStorage.clear();
-          this.toastr.info('',
-            environment.sesionexpiro
-          );
-          this.modal.dismissAll();
-          this.router.navigate(['/', 'login']);
+          if (error.status == 403) {
+            this.spinner.hide();
+            localStorage.clear();
+            this.toastr.info('', environment.sesionexpiro);
+            this.modal.dismissAll();
+            this.router.navigate(['/', 'login']);
+          }
         }
-      }
-    );
+      );
   }
 
-  getListPacientes(){
+  getListPacientes() {
     this.services.getPacientes().subscribe(
-      (result:any) => {
-        this.viewDateTableEspanol();
-        this.getDataOpcionesTable();
+      (result: any) => {
+        //this.viewDateTableEspanol();
         this.listPacientes = result;
 
+        this.getDataOpcionesTable();
       },
       (error) => {
-          console.log(`Lsita eror:  ${error}`);
+        console.log(`Lsita eror:  ${error}`);
       }
     );
   }
@@ -166,8 +172,8 @@ export class GestionPacientesComponent implements OnInit, OnDestroy {
     this.modal.dismissAll(modal);
   }
 
-   /**Opciones español datatable */
-   viewDateTableEspanol() {
+  /**Opciones español datatable */
+  viewDateTableEspanol() {
     this.dtOptions = {
       pagingType: 'full_numbers',
       pageLength: 5,
@@ -180,7 +186,7 @@ export class GestionPacientesComponent implements OnInit, OnDestroy {
 
   /**Datetabble */
   getDataOpcionesTable() {
-    this.viewDateTableEspanol();
+    //this.viewDateTableEspanol();
     if (this.isDtInitializeds) {
       this.dtElements.dtInstance.then((dtInstances: DataTables.Api) => {
         dtInstances.destroy();
@@ -192,5 +198,4 @@ export class GestionPacientesComponent implements OnInit, OnDestroy {
     }
     /** Fin datetabble */
   }
-
 }
