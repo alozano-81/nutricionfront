9dd44fdae26bb77fdf1fb22d79d4056c8e278ac0
Andres Lozano <yeisonandreslozano@gmail.com>
2024-11-01 20:32:03 -0500

    agregacion componente estilo de vida

---
+++ src/app/pages/gestion-estilo-vida/gestion-estilo-vida.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gestion-estilo-vida',
+  standalone: true,
+  imports: [],
+  templateUrl: './gestion-estilo-vida.component.html',
+  styleUrl: './gestion-estilo-vida.component.scss'
+})
+export class GestionEstiloVidaComponent {
+
+}
